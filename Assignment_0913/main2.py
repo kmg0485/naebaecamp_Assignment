edacd93f58642b7c2ffb97f1e43aacc4dfaf18d5
kmg0485 <kmg0485@naver.com>
2022-09-14 16:49:35 +0900

    0913 계산기 오류 수정

--- Assignment_0913/main2.py
+++ Assignment_0913/main2.py
@@ -1,3 +1,5 @@
+
+"""
 class Calc():
 
     def set_number(self, a, b):
@@ -15,3 +17,26 @@ print(calc.plus)
 print(calc.minus)
 print(calc.multiple)
 print(calc.divide) 
+"""
+class Calc():
+    
+    def set_number(self, a, b):
+        self.a = int(a)
+        self.b = int(b)
+        
+    def plus(self):
+        return f"더하기 : {self.a + self.b}"
+    def minus(self):
+        return f"빼  기 : {self.a - self.b}"
+    def multiple(self):
+        return f"곱하기 : {self.a * self.b}"
+    def divide(self):
+        return f"나누기 : {self.a / self.b}"
+    
+calc = Calc()   
+calc.set_number(20, 10)
+    
+print(calc.plus())
+print(calc.minus())
+print(calc.multiple())
+print(calc.divide()) 
