9405036ed827a56e12058ff62951e164f51a398a
kmg0485 <kmg0485@naver.com>
2022-09-15 14:37:33 +0900

    Assignment_0914 - 1번 과제 코드 수정

--- Assignment_0914/main1.py
+++ Assignment_0914/main1.py
@@ -1,4 +1,4 @@
-
+"""
 #calc.set_number 에 0이나 문자 넣어보기.
 try:
     class Calc():
@@ -17,7 +17,7 @@ try:
             return f"나누기 : {self.a / self.b}" 
         
     calc = Calc()   
-    calc.set_number(20, ㅁㅇㄴㄹㄹㅇㅁㄴㄹㅁㄴ)
+    calc.set_number(20, ㅁㄴㅇㄹㅁㄴㅇㄹ)
     
     print(calc.plus())
     print(calc.minus())
@@ -32,4 +32,46 @@ except ZeroDivisionError:
 except NameError: 
     print("숫자만 입력 가능합니다.")
     exit()
+"""
+###############################################################
 
+class Calc():
+
+    def set_number(self, a, b):
+        self.a = int(a)
+        self.b = int(b)
+            
+    def plus(self):
+        return f"더하기 : {self.a + self.b}"
+    def minus(self):
+        return f"빼  기 : {self.a - self.b}"
+    def multiple(self):
+        return f"곱하기 : {self.a * self.b}"
+    def divide(self):
+        try:
+            f"나누기 : {self.a / self.b}" 
+        except ZeroDivisionError: 
+            print("0으로는 나눌수 없습니다.")
+            exit()
+            
+            
+            
+while True:
+    try:
+        num1, num2 = [int(x) for x in input().split(" ")]
+        break
+    except ValueError:
+        print("숫자만 입력 가능합니다.")
+        exit()
+    
+    
+    
+calc = Calc()   
+calc.set_number(num1, num2)
+    
+print(calc.plus())
+print(calc.minus())
+print(calc.multiple())
+print(calc.divide()) 
+
+    
